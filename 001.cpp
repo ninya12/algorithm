6e75a832b5e7bd8460289d498d7d2837832bb134
ninya12 <ninya12@naver.com>
2019-09-04 15:06:09 +0900

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -3,16 +3,19 @@
 
 using namespace std;
 
+// a function that adds 2D vector arr1 and 2d vector arr2
 vector<vector<int> > solution(vector<vector<int> > arr1, vector<vector<int> > arr2)
 {
     int size1 = arr1.size();
     int size2 = arr1[0].size();
+    // 2D vector initialization 
     vector<vector<int> > answer(size1, vector<int>(size2, 0));
 
     for(int i=0;i<arr1.size();i++)
     {
         for(int j=0;j<arr1[0].size();j++)
         {
+            // add arr1 and arr2
             answer[i][j] = arr1[i][j] + arr2[i][j];
         }
     }
@@ -31,6 +34,7 @@ int main(){
 
     answer = solution(a, b);
 
+    // 2D vector print to standard output stream
     for(vector<int> vec : answer)
     {
         for(auto i:vec)
