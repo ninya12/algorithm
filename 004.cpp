15827c5f6eb2fa3bac951137ecd03d502714bda0
ninya12 <ninya12@naver.com>
2019-09-05 16:42:10 +0900

    Update 004.cpp

--- 004.cpp
+++ 004.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
+// 직사각형 만들기
 vector<int> solution(int brown, int red) {
     vector<int> answer({0, 0});
     int b;
-    for(int a=1; a<pow(red,0.5)+1; a++){
+    // O(N^1/2)
+    for(int a=1; a*a<=red; a++){
         if(red%a == 0){
+            // red = a * b
             b = red / a;
+            // a * b = red then entirety = (b+2) * (a+2) = red + 4 + 2(a+b) = red + brown
             if(2*(a+b)+4 == brown){
+                // width > length
                 if(b>a){
                     answer[0] = b+2;
                     answer[1] = a+2;
