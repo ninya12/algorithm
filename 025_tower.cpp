f686fabf3e8b0536b8100a6016161051f66d9b5e
ninya12 <ninya12@naver.com>
2019-09-28 23:37:39 +0900

    Create 025_tower.cpp

---
+++ 025_tower.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+// https://programmers.co.kr/learn/courses/30/lessons/42588
+// 탑
+
+using namespace std;
+
+vector<int> solution(vector<int> heights) {
+    vector<int> answer;
+    return answer;
+}
+
+int main(){
+    vector<int> tc = {6,9,5,7,4};
+    vector<int> ans = solution(tc);
+    for(auto i : ans) cout << i << " ";
+    cout << endl;
+    return 0;
+}
